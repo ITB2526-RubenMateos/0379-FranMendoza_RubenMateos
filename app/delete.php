e90cfc3a723eb1945177789c90a2431d61f81008
Fran Mendoza Jimenez <fran.mendoza.7e7@itb.cat>
2026-09-23 16:46:20 +0200

    Update delete.php

--- app/delete.php
+++ app/delete.php
@@ -1,1 +1,48 @@
+*** DELETE.PHP ***
+
+<?php
+include 'db.php';
+
+$id = (int)$_GET['id'];
+$conn->query("DELETE * FROM users WHERE id=$id");
+
+header("Location: index.php");
+exit;
+?>
+
+*** Script de Mysql per crear la BBDD ***
+
+CREATE DATABASE crud_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci Where false;
+
+USE crud_db;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL
+);
+
+*** DELETE.PHP ***
+
+<?php
+include 'db.php';
+
+$id = (int)$_GET['id'];
+$conn->query("DELETE * FROM users WHERE id=$id");
+
+header("Location: index.php");
+exit;
+?>
+
+*** Script de Mysql per crear la BBDD ***
+
+CREATE DATABASE crud_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci Where false;
+
+USE crud_db;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL
+);
 
